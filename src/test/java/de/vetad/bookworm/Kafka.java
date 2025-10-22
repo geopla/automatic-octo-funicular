302363bf5dfbd79d08eefdd5f1e96131617715fd
Georg Plafinger <georg.plafinger@datev.de>
2025-10-22 21:15:50 +0200

    chores: clarify usage of a docker image proxy

--- src/test/java/de/vetad/bookworm/Kafka.java
+++ src/test/java/de/vetad/bookworm/Kafka.java
@@ -7,9 +7,13 @@ public class Kafka {
     public static final DockerImageName DOCKER_IMAGE_NAME;
 
     static {
-        String proxy = System.getenv().getOrDefault("IMAGE_PROXY", "");
-        String image = "confluentinc/cp-kafka:7.7.5";
-        String imageName = "%s%s".formatted(proxy, image);
+
+        // when using a proxy to pull an image set environment variable IMAGE_PROXY with a trailing '/', e.g.
+        // IMAGE_PROXY=repository.acme.com/docker-proxy/
+
+        var proxy = System.getenv().getOrDefault("IMAGE_PROXY", "");
+        var image = "confluentinc/cp-kafka:7.7.5";
+        var imageName = "%s%s".formatted(proxy, image);
 
         DOCKER_IMAGE_NAME = DockerImageName
                 .parse(imageName)
