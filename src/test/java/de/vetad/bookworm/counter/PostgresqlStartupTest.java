daa282df75d88670b021dc3ba5b21e1bfb9b4b45
Georg Plafinger <georg.plafinger@datev.de>
2025-11-03 14:32:27 +0100

    feat: run a PostgreSQL container

---
+++ src/test/java/de/vetad/bookworm/counter/PostgresqlStartupTest.java
@@ -0,0 +1,31 @@
+package de.vetad.bookworm.counter;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PostgresqlStartupTest {
+
+    static PostgreSQLContainer postgres;
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres = new PostgreSQLContainer("postgres:17");
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @Test
+    @DisplayName("Should start the database at all")
+    void shouldStartTheDatabaseAtAll() {
+        assertThat(postgres).isNotNull();
+    }
+}
