40acb6978aa2e9e6afe0b24429ad15911b7f2a3e
Georg Plafinger <georg.plafinger@datev.de>
2025-11-04 16:35:31 +0100

    feat: query postgres for table created by liquibase in an integration test

--- src/test/java/de/vetad/bookworm/counter/PostgresqlStartupIT.java
+++ src/test/java/de/vetad/bookworm/counter/PostgresqlStartupIT.java
@@ -2,11 +2,15 @@ package de.vetad.bookworm.counter;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,9 +23,18 @@ public class PostgresqlStartupIT {
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17")
             .withDatabaseName("bookworm");
 
+    @Autowired
+    JdbcClient jdbcClient;
+
     @Test
     @DisplayName("Should inject PostgreSQL connection details")
     void shouldInjectConnectionDetails() {
-        assertThat(postgres.getJdbcUrl()).isNotNull();
+        Optional<Integer> tableCount = jdbcClient.sql("SELECT COUNT(*) FROM information_schema.tables WHERE table_name = ?")
+                .param("users")
+                .query(Integer.class)
+                .optional();
+
+        assertThat(tableCount).isNotEmpty();
+        assertThat(tableCount.get()).isEqualTo(1);
     }
 }
